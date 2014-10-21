9c8b69a5c8ab42eb3a8843e6a9b22d20598b12f0
Atidote <atidote63240@gmail.com>
2014-10-21 15:05:27 +0200

    CLASSE

--- Babel_Serveur/Babel_Serveur/CPP/Client.cpp
+++ Babel_Serveur/Babel_Serveur/CPP/Client.cpp
@@ -11,3 +11,31 @@ Client::Client(string Username)
 {
 	this->Name = Username;
 }
+Client::~Client()
+{
+	CONTACTIT IT = myList.begin();
+	while (IT != myList.end())
+	{
+		myList.erase(IT);
+	}
+}
+Client::CONTACTLIST	Client::getContact()
+{
+	return this->myList;
+}
+bool			Client::addClient(const std::string &pseudo)
+{
+  
+}
+bool			Client::removeClient(const std::string &pseudo)
+{
+
+}
+Client*			Client::getClient()
+{
+	return this;
+}
+bool			Client::findClient(const std::string &pseudo)
+{
+
+}
--- Babel_Serveur/Babel_Serveur/Include/Client.h
+++ Babel_Serveur/Babel_Serveur/Include/Client.h
@@ -1,16 +1,29 @@
+#include <list>
+#include <iostream>
+#include <map>
+
 #ifndef CLIENT
 # define CLIENT
 
+using namespace std;
+
 class Client
 {
-public:
-	Client(string Username);
-	~Client(){};
 private:
-	string	Name;
 	typedef std::list<string> CONTACTLIST;
 	typedef CONTACTLIST::value_type CONTACTVAL;
 	typedef CONTACTLIST::iterator CONTACTIT;
+	CONTACTLIST	myList;
+public:
+	Client(string Username);
+	~Client();
+	CONTACTLIST	getContact();
+private:
+	string	Name;
+	bool				addClient(const std::string &);
+	bool				removeClient(const std::string &);
+	Client*				getClient();
+	bool				findClient(const std::string &);
 };
 
 #endif
