8a4d39fd750faa6bfff6531aac97c0c03f85b52d
Loïc <loic@loic-HP-EliteBook-8560p.(none)>
2014-10-22 17:17:10 +0200

    function tab weshmaggle

--- Babel_Serveur/Babel_Serveur/CPP/Protocole.cpp
+++ Babel_Serveur/Babel_Serveur/CPP/Protocole.cpp
@@ -2,82 +2,59 @@
 
 Protocole::Protocole()
 {
+  func_tab[0] = Protocole::Welcome;
+  func_tab[1] = Protocole::Contact_List;
+  func_tab[2] = Protocole::Contact_Add;
+  func_tab[3] = Protocole::Contact_Remove;
+  func_tab[4] = Protocole::Contact_Call_Me;
+  func_tab[5] = Protocle::Contact_To_Call;
+  func_tab[6] = Protocole::Authentification;
 }
+
 Protocole::~Protocole()
 {
 }
-void		Protocole::Welcome(const std::string pseudo)
+
+void		Protocole::Welcome(const std::string &pseudo)
 {
 	std::cout << "Welcome" << std::endl;
 }
-void		Protocole::Contact_List(const std::string pseudo)
+
+void		Protocole::Contact_List(const std::string &pseudo)
 {
 	std::cout << "Contact_List" << std::endl;
 }
-void		Protocole::Contact_Add(const std::string pseudo)
+
+void		Protocole::Contact_Add(const std::string &pseudo)
 {
 	std::cout << "Contact_Add" << std::endl;
 }
-void		Protocole::Contact_Remove(const std::string pseudo)
+
+void		Protocole::Contact_Remove(const std::string &pseudo)
 {
 	std::cout << "Contact_Remove" << std::endl;
 }
-void		Protocole::Contact_Call_Me(const std::string pseudo)
+
+void		Protocole::Contact_Call_Me(const std::string &pseudo)
 {
 	std::cout << "Contact_Call_Me" << std::endl;
 }
-void		Protocole::Contact_To_Call(const std::string pseudo)
+
+void		Protocole::Contact_To_Call(const std::string &pseudo)
 {
 	std::cout << "Contact_To_Call" << std::endl;
 }
-void		Protocole::Authentification(const std::string pseudo)
+
+void		Protocole::Authentification(const std::string &pseudo)
 {
 	std::cout << "Auth" << std::endl;
 }
-void		Protocole::Protocole_to_call(const int id, const std::string str)
+
+void		Protocole::Protocole_to_call(const int id, const std::string &str)
 {
-	std::cout << id << std::endl;
-	switch (id)
-	{
-		case SALUT:
-		{
-			this->Welcome(str);
-			break;
-		}
-		case CONTACT_LIST:
-		{
-			this->Contact_List(str);
-			break;
-		}
-		case CONTACT_ADD:
-		{
-			this->Contact_Add(str);
-			break;
-		}
-		case CONTACT_REMOVE:
-		{
-			this->Contact_Remove(str);
-			break;
-		}
-		case CONTACT_CALL_ME:
-		{
-			this->Contact_Call_Me(str);
-			break;
-		}
-		case CONTACT_TO_CALL:
-		{
-			this->Contact_To_Call(str);
-			break;
-		}
-		case AUTH:
-		{
-			this->Authentification(str);
-			break;
-		}
-		default:
-		{
-			Logger::Instance()->log(2, "Protocole: Unknow enum\n");
-			break;
-		}
-	}
+  std::cout << id << std::endl;
+  if (id - 1 > 6)
+    Logger::Instance()->log(2, "Protocole: Unknow enum\n");
+  else
+    func_tab[id - 1](str);    
 }
--- Babel_Serveur/Babel_Serveur/Include/Protocole.h
+++ Babel_Serveur/Babel_Serveur/Include/Protocole.h
@@ -12,7 +12,10 @@ struct				Proto_Struct
 	char			Buffer[512];
 	int				audio;
 };
+
 typedef				Proto_Struct Struct_Proto;
+typedef				void (Protocole::*ptr)(const std::string &);
+
 class				Protocole
 {
 private:
@@ -28,18 +31,19 @@ private:
 		CONTACT_TO_CALL,
 		PROTOCOLE_END
 	};
+	ptr			func_tab[7];
 public:
 	Protocole();
 	~Protocole();
-	void			Protocole_to_call(const int id, std::string str);
+	void			Protocole_to_call(const int id, const std::string &str);
 private:
-	void			Welcome(std::string pseudo);
-	void			Contact_List(std::string pseudo);
-	void			Contact_Add(std::string pseudo);
-	void			Contact_Remove(std::string pseudo);
-	void			Contact_Call_Me(std::string pseudo);
-	void			Contact_To_Call(std::string pseudo);
-	void			Authentification(std::string pseudo);
+	void			Welcome(const std::string &pseudo);
+	void			Contact_List(const std::string &pseudo);
+	void			Contact_Add(const std::string &pseudo);
+	void			Contact_Remove(const std::string &pseudo);
+	void			Contact_Call_Me(const std::string &pseudo);
+	void			Contact_To_Call(const std::string &pseudo);
+	void			Authentification(const std::string &pseudo);
 };
 
 #endif // PROTOCOLE
