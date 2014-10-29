e6a1c17a88ec71b0691c63cbe3f35b24df8fa1fd
GARCIA Benjamin <benjamin.garcia@epitech.eu>
2014-10-29 14:25:10 +0100

    Add Audio test

---
+++ Babel_Client/inc/AudioHandler.h
@@ -0,0 +1,31 @@
+#include "portaudio.h"
+
+#include <iostream>
+#include <string>
+
+#define NUM_SECONDS   (4)
+#define SAMPLE_RATE   (44100)
+#define FRAMES_PER_BUFFER (1024)
+#define SAMPLE_TYPE paInt24
+#define NUM_CHANNELS    (2)
+
+class AudioHandler
+{
+public:
+	AudioHandler();
+	~AudioHandler();
+
+	bool 	initializePa();
+	bool 	initStream();
+	bool 	stopStream();
+	bool	closeStream();
+	bool	initChannels();
+
+protected:
+	PaStream  				*_stream;
+	PaError  				err;
+	int 					_readBufferSize;
+	PaStreamParameters		_inputChannel;
+	PaStreamParameters		_outputChannel;
+	unsigned char			*_readBuffer;
+};
---
+++ Babel_Client/src/AudioHandler.cpp
@@ -0,0 +1,112 @@
+#include "./inc/AudioHandler.h"
+
+AudioHandler::AudioHandler()
+{
+  _stream = NULL;
+  _readBufferSize = FRAMES_PER_BUFFER * NUM_CHANNELS * SAMPLE_SIZE;
+  _readBuffer= new unsigned char [_readBufferSize];
+}
+
+AudioHandler::~AudioHandler()
+{
+
+}
+
+bool 	AudioHandler::initializePa()
+{
+	err = Pa_Initialize();
+	if(err != paNoError)
+		return (false);
+	return (true);
+}
+
+bool 	AudioHandler::initStream()
+{
+	if ((Pa_OpenStream(&_stream, &_inputChannel, &_outputChannel, SAMPLE_RATE,
+		FRAMES_PER_BUFFER, paClipOff, NULL, NULL)) != paNoError)
+		std::cerr << "Unable to open stream" << std::endl;
+	err = Pa_StartStream(_stream);
+	if(err != paNoError)
+		std::cerr << "Unable to start stream" <<std::endl;
+	return (true);
+}
+
+bool 	AudioHandler::stopStream()
+{
+	err = Pa_StopStream(_stream);
+	if(err != paNoError)
+		return (false);
+	return (true);
+}
+
+bool 	AudioHandler::closeStream()
+{
+	err = Pa_CloseStream(_stream);
+	if(err != paNoError)
+		return (false);
+	return (true);
+}
+
+bool 	AudioHandler::terminatePa()
+{
+	err = Pa_Terminate();
+	if(err != paNoError)
+		return (false);
+	return (true);
+}
+
+bool 	AudioHandler::initChannels()
+{
+	if ((initPa()) == false)
+		std::cerr << "Unable to initialize PortAudio" << std::endl;
+	if ((_inputChannel.device = Pa_GetDefaultInputDevice()) != paNoDevice)
+	{
+		_inputChannel.channelCount = 2;
+		_inputChannel.hostApiSpecificStreamInfo = NULL;
+		_inputChannel.sampleFormat = SAMPLE_TYPE;
+		_inputChannel.suggestedLatency = Pa_GetDeviceInfo(_inputParam.device)->defaultLowInputLatency;
+	}
+	if ((_outputParam.device = Pa_GetDefaultInputDevice()) != paNoDevice)
+	{
+		_outputParam.channelCount = 2;
+		_outputParam.hostApiSpecificStreamInfo = NULL;
+		_outputParam.sampleFormat = SAMPLE_TYPE;
+		_outputParam.suggestedLatency = Pa_GetDeviceInfo(_outputParam.device)->defaultLowInputLatency;
+	}
+	return (true);
+}
+
+/**********************************************/
+
+bool            AudioHandler::writeStream(unsigned char *buff)
+{
+	if ((Pa_WriteStream(_stream, buff, FRAMES_PER_BUFFER)) != paNoError)
+	{
+		std::cout << "Failed to write on stream" <<std::endl;
+		return (false);
+	}
+	return (true);
+}
+
+bool            AudioHandler::readStream()
+{
+	if ((Pa_ReadStream(_stream, _readBuffer, FRAMES_PER_BUFFER)) != paNoError)
+		return (false);
+	return (true);
+}
+
+void            AudioHandler::cleanReadBuffer()
+{
+	for (int i = 0; i != _readBufferSize; i++)
+		_readBuffer[i] = 0;
+}
+
+int             AudioHandler::getReadBufferSize() const
+{
+	return (_readBufferSize);
+}
+
+unsigned char*          AudioHandler::getReadBuffer() const
+{
+	return (_readBuffer);
+}
