a0db61fca95c4cedd7a1b87ffa3c9111b1288e87
GARCIA Benjamin <benjamin.garcia@epitech.eu>
2014-11-08 00:08:54 +0100

    Modif client.cpp

--- Babel_Client/Client-Server/Client.cpp
+++ Babel_Client/Client-Server/Client.cpp
@@ -43,13 +43,9 @@ void		Client::run()
 		//socket->send("salut", "127.0.0.1", SERVER_PORT);
 		Struct_Proto t;
 
-		strcpy_s(t.Buffer, "");
-		t.EnumId = 1;
-		socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
-		socket->recvFromSomeone(ip, SERVER_PORT, &t);
-		printf("%d, %s, %d\n", t.audio, t.Buffer, t.EnumId);
 	    if (strcmp(t.Buffer, WELCOME) == 0)
 		{
+
 			memset(&t, 0, sizeof(t));
 			t.audio = 1;
 			strcpy_s(t.ip,"127.0.0.1");
@@ -137,34 +133,110 @@ bool 	Client::connectToServer()
 
 bool 	Client::authToServer()
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, "Atidote");
+	t.EnumId = AUTH;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, "127.0.0.1");
+	t.EnumId = AUTH;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	return (true);
 }
 
 bool 	Client::getContactList()
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, "");
+	t.EnumId = CONTACT_LIST;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (t.Buffer == NULL)
+		return (false);
+	return (true);
 }
 
-bool 	Client::addContact()
+bool 	Client::addContact(char *contact)
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, contact);
+	t.EnumId = CONTACT_ADD;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	return (true);
 }
 
-bool 	Client::suppressContact()
+bool 	Client::suppressContact(char *contact)
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, contact);
+	t.EnumId = CONTACT_REMOVE;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	return (true);
 }
 
-bool 	Client::callContact()
+bool 	Client::callContact(char *contact)
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, contact);
+	t.EnumId = CONTACT_TO_CALL;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	return (true);
 }
 
-bool	Client::acceptCall()
+bool	Client::acceptCall(char *contact)
 {
+/*	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, "");
+	t.EnumId = CONTACT_CALL_ME;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	return (true);*/
 }
 
 bool 	Client::hangUpCall()
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, "");
+	t.EnumId = CONTACT_CALL_REFUSE;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	memset(&t, 0, sizeof(t));
+	socket->recvFromSomeone(ip, SERVER_PORT, &t);
+	if (strcmp(t.Buffer, "ok") != 0)
+		return (false);
+	return (true);
 }
 
 bool 	Client::disconnectFromServer()
 {
+	memset(&t, 0, sizeof(t));
+	strcpy_s(t.Buffer, contact);
+	t.EnumId = DISCONNECT;
+	socket->sendToSomeone(&t, "127.0.0.1", SERVER_PORT);
+	return (true);
 }
 
 bool	Client::recv_socket(string &data)
